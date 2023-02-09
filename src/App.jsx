984ba9b2c298a2fb6c8b773164d343c6789efbd3
Daniel Romero <romerohoyosd@gmail.com>
2023-02-08 22:31:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navigation } from './components/Navigation';
+
+function App() {
+  return <Navigation />;
+}
+
+export default App;
---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const HomePage = () => {
+  return <h2>Hola Devs</h2>;
+};
